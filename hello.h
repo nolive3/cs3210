81a6441f48826f0e2bd16c30e1d633aeecda8e21
Jeremy <jgrebnergt@gmail.com>
2012-11-28 13:31:51 -0500

    Added header file for main program

---
+++ hello.h
@@ -0,0 +1,13 @@
+const char* dates_str = "^/Dates$";
+const char* dates_year_str = "^/Dates/[0-9]{4}$";
+const char* dates_year_month_str = "^/Dates/[0-9]{4}/(January|February|March|April|May|June|July|August|September|October|November|December)$";
+const char* all_str = "^/All$";
+const char* formats_str = "^/Formats$";
+const char* formats_ext_str = "^/Formats/(jpg|jpeg|png|bmp|tiff|tif)$";
+
+regex_t dates_rx;
+regex_t dates_year_rx;
+regex_t dates_year_month_rx;
+regex_t all_rx;
+regex_t formats_rx;
+regex_t formats_ext_rx;
